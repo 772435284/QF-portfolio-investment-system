Fix reward and QPL calculation, seem trainable

QFPIS_backtest.py
@@ -10,12 +10,9 @@ import torch.nn.functional as F
 from api_types import GlobalConfig, AgentProps
 import math
 import yaml
-from torch.distributions import Categorical
 from utils.utils import normalize,load_observations
 from tools.ddpg.ornstein_uhlenbeck import OrnsteinUhlenbeckActionNoise
-from models.DDPG import Actor
-from models.QFPIS import Policy
-from utils.utils import obs_normalizer
+
 from environment.QF_env import envs
 from typing import Callable, List, cast, OrderedDict
 from backtestor import backtestor
@@ -27,7 +24,7 @@ env = envs(config)
 backtestor = backtestor(env, OrnsteinUhlenbeckActionNoise, device, config)
 
 backtestor.load_actor()
-backtestor.load_policy(action_size=config.qpl_level)
+backtestor.load_policy(action_size=config.qpl_level+1)
 
 CR = backtestor.backtest_qf(backtestor.actor, backtestor.policy)
 print(CR)

backtestor.py
@@ -15,9 +15,9 @@ from utils.utils import normalize,load_observations
 from tools.ddpg.ornstein_uhlenbeck import OrnsteinUhlenbeckActionNoise
 from models.DDPG import Actor
 from models.QFPIS import Policy
-from utils.utils import obs_normalizer
 from environment.env import envs
 from typing import Callable, List, cast, OrderedDict
+from observation.obs_creator import obs_creator
 
 
 class backtestor(object):
@@ -37,22 +37,25 @@ class backtestor(object):
         self.feature_num = len(self.market_feature)
         self.steps = config.max_step
         self.mode = config.mode
+        self.action_size = config.qpl_level + 1
+        self.num_features = len(config.factor)
     
     
 
     def load_actor(self):
-        self.actor = Actor(product_num=self.product_num,win_size=self.window_size).to(self.device)
-        self.actor.load_state_dict(torch.load(path_join(self.config.ddpg_model_dir, AgentProps(self.config.agent_list[self.agent_index]).name +'_0'+ str(self.config.episode-1))))
+        self.actor = Actor(product_num=self.product_num,win_size=self.window_size,num_features=self.num_features).to(self.device)
+        self.actor.load_state_dict(torch.load(path_join(self.config.ddpg_model_dir, AgentProps(self.config.agent_list[self.agent_index]).name +'_'+ str(self.config.episode-1))))
         
 
     def load_policy(self,action_size):
-        self.policy = Policy(product_num = self.product_num, win_size = self.window_size, action_size = action_size).to(self.device)
-        self.policy.load_state_dict(torch.load(path_join(self.config.pga_model_dir, AgentProps(self.config.agent_list[self.agent_index]).name +'_0'+ str(self.config.episode-1))))
+        self.policy = Policy(product_num = self.product_num, win_size = self.window_size,num_features=self.num_features, action_size = action_size).to(self.device)
+        self.policy.load_state_dict(torch.load(path_join(self.config.pga_model_dir, AgentProps(self.config.agent_list[self.agent_index]).name +'_'+ str(self.config.episode-1))))
         
 
     def backtest_ddpg(self, model):
+        creator = obs_creator(self.config.norm_method,self.config.norm_type)
         observation, info = self.env.reset()
-        observation = obs_normalizer(observation)
+        observation = creator.create_obs(observation)
         observation = observation.transpose(2, 0, 1)
         done = False
         ep_reward = 0
@@ -67,16 +70,17 @@ class backtestor(object):
             r = info['log_return']
             wealth=wealth*math.exp(r)
             CR.append(wealth)
-            observation = obs_normalizer(observation)
+            observation =  creator.create_obs(observation)
             observation = observation.transpose(2, 0, 1)
         return CR
 
     def backtest_qf(self, actor, policy):
+        creator = obs_creator(self.config.norm_method,self.config.norm_type)
         eps = 1e-8
         actions = []
         weights = []
         observation, info = self.env.reset()
-        observation = obs_normalizer(observation)
+        observation = creator.create_obs(observation)
         observation = observation.transpose(2, 0, 1)
         done = False
         ep_reward = 0
@@ -100,7 +104,7 @@ class backtestor(object):
             wealth=wealth*math.exp(r)
             CR.append(wealth)
             ep_reward += reward
-            observation = obs_normalizer(observation)
+            observation = creator.create_obs(observation)
             observation = observation.transpose(2, 0, 1)
         return actions, weights, CR
 

environment/QF_env.py
@@ -54,63 +54,84 @@ class envs(gym.Env):
         # Connect 1, no risk asset to the portfolio
         c_observation = np.ones((1, self.window_length, observation.shape[2]))
         observation = np.concatenate((c_observation, observation), axis=0)
-
         
         open_price_vector = observation[:, -1, 0]
         high_price_vector = observation[:, -1, 1]
         low_price_vector = observation[:, -1 , 2]
         close_price_vector = observation[:, -1, 3]
-
         # Price relative vector
-        pr = observation[:, 0, 3] / observation[:, -1, 3]
         
-        # Use qpl levels
-        combine_qpl = []
-        for i in range(self.qpl_level):
-            QPL_vector = open_price_vector * self.combined_nqpr[:,i]
-            combine_qpl.append(QPL_vector)
-        combine_qpl = np.array(combine_qpl)
-
+        QPL1_vector = open_price_vector * self.combined_nqpr[:,0]
+        
+        QPL2_vector = open_price_vector * self.combined_nqpr[:,1]
+        
+        # combine_qpl = []
+        # for i in range(self.qpl_level):
+        #     QPL_vector = open_price_vector * self.combined_nqpr[:,i]
+        #     combine_qpl.append(QPL_vector)
+        # combine_qpl = np.array(combine_qpl)
+        
         reset = 0
         y1 = np.zeros((10,), dtype=float)
         
         y1[0] = close_price_vector[0]/open_price_vector[0]
         for i in range(1, len(open_price_vector)):
-            for j in range(len(combine_qpl)):
-                # Select action 0
-                # Buy and hold
-                if action_policy == 0:
-                    y1[i] = pr[i]
-                # Select action 1: choose QPL+1
-                # for the + QPL in range
-                if combine_qpl[j][i] < high_price_vector[i] and combine_qpl[j][i]>low_price_vector[i] and combine_qpl[j][i]!=0 and action_policy==j+1:
-                    y1[i] = combine_qpl[j][i]/close_price_vector[i]
-                    reset = 1
-                # for the +QPL not in range
-                if combine_qpl[j][i] > high_price_vector[i] and action_policy==j+1:
-                    y1[i] = pr[i]
+            # Select action 1: no reward
+            # Buy and hold
+            if action_policy==0:
+                y1[i] = close_price_vector[i]/open_price_vector[i]
+            # Select action 2: choose +QPL
+            # Open Buy Order
+            # for the + QPL in range
+            if QPL1_vector[i] < high_price_vector[i] and QPL1_vector[i]>low_price_vector[i] and QPL1_vector[i]!=0 and action_policy==1:
+                
+                y1[i] = QPL1_vector[i]/open_price_vector[i]
+            # for the +QPL not in range
+            if QPL1_vector[i] > high_price_vector[i] and action_policy==1:
+                
+                y1[i] = close_price_vector[i]/open_price_vector[i]
             
-           
+            if QPL2_vector[i] < high_price_vector[i] and QPL2_vector[i]>low_price_vector[i] and QPL2_vector[i]!=0 and action_policy==2:
+                y1[i] = QPL2_vector[i]/open_price_vector[i]
+                
+            if QPL2_vector[i] > high_price_vector[i] and action_policy==2:
+                y1[i] = close_price_vector[i]/open_price_vector[i]
+            
         open_price_vector = observation[:, -1, 0]
         high_price_vector = observation[:, -1, 1]
         low_price_vector = observation[:, -1 , 2]
         close_price_vector = observation[:, -1, 3]
         
+        QPL1_vector = open_price_vector * self.combined_nqpr[:,0]
+        
+        QPL2_vector = open_price_vector * self.combined_nqpr[:,1]
+        
+        c1 = close_price_vector / open_price_vector
         
         policy_reward = np.zeros((10,1), dtype=float)
+
         for i in range(len(open_price_vector)):
-            for j in range(len(combine_qpl)):
-                if action_policy == 0:
-                    policy_reward[i] = close_price_vector[i]-open_price_vector[i]
-                # Select action 1: choose QPL+1
-                # for the + QPL in range
-                if combine_qpl[j][i] < high_price_vector[i] and combine_qpl[j][i]>low_price_vector[i] and combine_qpl[j][i]!=0 and action_policy==j+1:
-                    policy_reward[i] = combine_qpl[j][i]-open_price_vector[i]
-                # for the +QPL not in range
-                if combine_qpl[j][i] > high_price_vector[i] and action_policy==j+1:
-                    policy_reward[i] = close_price_vector[i]-open_price_vector[i]
+            # Select action 0
+            # Buy and hold
+            if action_policy == 0:
+                policy_reward[i] = close_price_vector[i]-open_price_vector[i]
             
-        
+            # Select action 1: choose +QPL1
+            # for the + QPL1 in range
+            if QPL1_vector[i] < high_price_vector[i] and QPL1_vector[i]>low_price_vector[i] and QPL1_vector[i]!=0 and action_policy==1:
+                policy_reward[i] = QPL1_vector[i]-open_price_vector[i]
+            # for the + QPL1 not in range
+            if QPL1_vector[i] > high_price_vector[i] and action_policy==1:
+                policy_reward[i] = close_price_vector[i]-open_price_vector[i]
+            
+            # Select action 2: choose +QPL2
+            # for the + QPL2 in range
+            if QPL2_vector[i] < high_price_vector[i] and QPL2_vector[i]>low_price_vector[i] and QPL2_vector[i]!=0 and action_policy==2:
+                policy_reward[i] = QPL2_vector[i]-open_price_vector[i]
+            # for the + QPL2 not in range
+            if QPL2_vector[i] > high_price_vector[i] and action_policy==2:
+                policy_reward[i] = close_price_vector[i]-open_price_vector[i]
+            
         policy_reward = np.dot(weights, policy_reward)
         policy_reward = np.sum(policy_reward)
         

environment/portfolio.py
@@ -8,51 +8,60 @@ eps = 1e-8
 # A class that is to perform the calculation of the portfolio
 class Portfolio(object):
 
-    def __init__(self, steps, trading_cost, mode):
-
-        self.steps = steps
+    def __init__(self, steps, trading_cost, mode,time_cost=0.0):
+        #self.asset_names = asset_names
         self.cost = trading_cost
+        self.time_cost = time_cost
+        self.steps = steps
         self.mode = mode
 
     def _step(self, w1, y1, reset):
+        """
+        Step.
+        w1 - new action of portfolio weights - e.g. [0.1,0.9,0.0]
+        y1 - price relative vector also called return
+            e.g. [1.0, 0.9, 1.1]
+        Numbered equations are from https://arxiv.org/abs/1706.10059
+        """
+        assert w1.shape == y1.shape, 'w1 and y1 must have the same shape'
+        assert y1[0] == 1.0, 'y1[0] must be 1'
 
-        assert w1.shape == y1.shape, 'w1 and y1 must have same number of products'
-        assert y1[0] == 1.0, 'y1[0] should be 1'
-
-        w0 = self.w0
         p0 = self.p0
-        y0 = self.y0
-        dw1 = (y0 * w0) / (np.dot(y0, w0) + eps) 
-        
+
+        dw1 = (y1 * w1) / (np.dot(y1, w1) + eps)  # (eq7) weights evolve into
+
         if self.mode == "Test" and reset == 1:
             mu1 = self.cost * (np.abs(w1[1:])).sum()
         else:
             mu1 = self.cost * (np.abs(dw1[1:] - w1[1:])).sum()
-        
-        p1 = p0 * (1 - mu1) * np.dot(y1, w1)   
 
-        rho1 = p1 / p0 - 1  
-        r1 = np.log((p1 + eps) / (p0 + eps))  
-        reward = r1 / self.steps * 1000.  
-        
-        self.w0 = w1
+        assert mu1 < 1.0, 'Cost is larger than current holding'
+
+        p1 = p0 * (1 - mu1) * np.dot(y1, w1)  # (eq11) final portfolio value
+
+        p1 = p1 * (1 - self.time_cost)  # we can add a cost to holding
+
+        rho1 = p1 / p0 - 1  # rate of returns
+        r1 = np.log((p1 + eps) / (p0 + eps))  # log rate of return
+        reward = r1 / self.steps * 1000.  # (22) average logarithmic accumulated return
+        # remember for next step
         self.p0 = p1
-        self.y0 = y1
 
-        # Run out of money, done
+        # if we run out of money, we're done (losing all the money)
         done = p1 == 0
 
         info = {
+            "reward": reward,
+            "log_return": r1,
             "portfolio_value": p1,
             "rate_of_return": rho1,
-            "log_return": r1,
+            "weights_std": w1.std(),
+            "cost": mu1,
         }
         self.infos.append(info)
         return reward, info, done
 
+
     def reset(self):
-        self.w0 = np.array([1.0] + [0.0] * 9)
         self.infos = []
         self.p0 = 1.0
-        self.y0 = np.zeros((10,), dtype=float)
-        self.y0[0] = 1

models/DDPG.py
@@ -14,13 +14,14 @@ from os.path import join as path_join
 from torch.distributions import Categorical
 from utils.utils import hidden_init
 from tensorboardX import SummaryWriter
+from observation.obs_creator import obs_creator
 
 # Define actor network
 class Actor(nn.Module):
-    def __init__(self,product_num, win_size):
+    def __init__(self,product_num, win_size, num_features):
         super(Actor, self).__init__()
         self.conv1 = nn.Conv2d(
-            in_channels =  1,
+            in_channels =  num_features,
             out_channels = 32,
             kernel_size = (1,3),
             #stride = (1,3)
@@ -59,10 +60,10 @@ class Actor(nn.Module):
 
 # Define Critic network
 class Critic(nn.Module):
-    def __init__(self, product_num, win_size):
+    def __init__(self, product_num, win_size,num_features):
         super(Critic, self).__init__()
         self.conv1 = nn.Conv2d(
-            in_channels =  1,
+            in_channels =  num_features,
             out_channels = 32,
             kernel_size = (1,3),
             #stride = (1,3)
@@ -125,16 +126,17 @@ class DDPG(object):
         self.actor_noise = actor_noise
         self.env = env
         self.device = device
+        self.num_features = len(config.factor)
         # self.price_history = price_history
         # self.trading_dates = trading_dates
         # assert len(trading_dates) == config.max_step+self.window_size
         
         self.summary_path = path_join(config.summary_path, config.model_name)
         
-        self.actor = Actor(product_num,window_size).to(self.device)
-        self.actor_target = Actor(product_num,window_size).to(self.device)
-        self.critic = Critic(product_num,window_size).to(self.device)
-        self.critic_target = Critic(product_num,window_size).to(self.device)
+        self.actor = Actor(product_num,window_size,self.num_features).to(self.device)
+        self.actor_target = Actor(product_num,window_size,self.num_features).to(self.device)
+        self.critic = Critic(product_num,window_size,self.num_features).to(self.device)
+        self.critic_target = Critic(product_num,window_size,self.num_features).to(self.device)
         
         self.actor.reset_parameters()
         self.actor_target.reset_parameters()
@@ -187,11 +189,12 @@ class DDPG(object):
         self.buffer = ReplayBuffer(self.config.buffer_size)
         total_step = 0
         writer = SummaryWriter(logdir=self.summary_path)
+        creator = obs_creator(self.config.norm_method,self.config.norm_type)
         # Main training loop
         for i in range(num_episode):
-            previous_observation = self.env.reset()
+            previous_observation, _ = self.env.reset()
             # Normalization
-            previous_observation = obs_normalizer(previous_observation)
+            previous_observation = creator.create_obs(previous_observation)
             # Reshape
             previous_observation = previous_observation.transpose(2, 0, 1)
             ep_reward = 0
@@ -207,7 +210,7 @@ class DDPG(object):
         		# 2. Obtain reward rt and reach new state st+1
                 # ================================================
                 observation, reward, done, _ = self.env.step(action)
-                observation = obs_normalizer(observation)
+                observation = creator.create_obs(observation)
                 # Reshape
                 observation = observation.transpose(2, 0, 1)
                 # ================================================

models/QFPIS.py
@@ -15,7 +15,7 @@ from torch.distributions import Categorical
 from utils.utils import hidden_init
 from tensorboardX import SummaryWriter
 from observation.obs_creator import obs_creator
-import torchlayers
+
 
 # Define actor network
 class Actor(nn.Module):