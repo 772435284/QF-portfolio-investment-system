38227e0085dfdc111dae12f86fc5146ad3a70648
Yitao Qiu <772435284@qq.com>
2023-04-14 16:02:45 +0800

    Use new data for training

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -90,16 +90,7 @@ class Dataset_Custom(object):
             if os.path.exists(file_path):
                 stock_data = pd.read_csv(file_path)
                 
-                # Important: Make the date is in ascending order for QPL calcualtion!!!
-                # 先将数据按正序排序，取出训练集
-                # Combine the 'Year', 'Month', and 'Day' columns to form a single 'Date' column and convert it to datetime format
-                stock_data['Date'] = pd.to_datetime(stock_data[['Year', 'Month', 'Day']])
-
-                # Drop the 'Year', 'Month', and 'Day' columns
-                stock_data = stock_data.drop(columns=['Year', 'Month', 'Day'])
-
-                # Sort the DataFrame based on the 'Date' column in ascending order
-                stock_data = stock_data.sort_values(by='Date')
+                stock_data = stock_data[::-1]
                 
                 # 提取收盘价数据
                 close_prices = stock_data['Close'].tolist()
